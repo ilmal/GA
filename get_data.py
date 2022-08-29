92c459b1eb962da15408e5a2f153fc50008a1258
ilmal <69248971+ilmal@users.noreply.github.com>
2022-08-29 10:31:39 +0200

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -13,26 +13,7 @@ from addict import Dict
 from PIL import Image
 import os
 
-def get_data():
-  train_config = Dict({
-      "path": "../input/100-bird-species/train",
-      "shape": (224, 224),
-      "batch_size": 16,
-      "num_workers": 4,
-      "augmentations": A.Compose([
-          A.HorizontalFlip(p=0.5),
-          ToTensor(),
-      ])
-  })
-
-
-  validation_config = Dict({
-      "path": "../input/100-bird-species/valid",
-      "shape": (224, 224),
-      "batch_size": 32,
-      "num_workers": 4,
-      "augmentations": None,
-  })
+def get_data(train_config, validation_config):
 
   class BirdsDataset(Dataset):
       def __init__(self, path, augmentations=None, shape=(224, 224)):
