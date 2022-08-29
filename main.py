2c3f70cad230cd9e629fb5d273b04bbaa7c8d9c5
ilmal <69248971+ilmal@users.noreply.github.com>
2022-08-29 10:30:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+from get_data import get_data
 
+train_config = Dict({
+  "path": "../input/100-bird-species/train",
+  "shape": (224, 224),
+  "batch_size": 16,
+  "num_workers": 4,
+  "augmentations": A.Compose([
+      A.HorizontalFlip(p=0.5),
+      ToTensor(),
+  ])
+})
+
+
+validation_config = Dict({
+  "path": "../input/100-bird-species/valid",
+  "shape": (224, 224),
+  "batch_size": 32,
+  "num_workers": 4,
+  "augmentations": None,
+})
+
+
+def main():
+
+  get_data(train_config, validation_config)
+  
+  
+  
+def start_runtime():
+  main()
+  
+if __name__ == "__main__":
+  start_runtime()
